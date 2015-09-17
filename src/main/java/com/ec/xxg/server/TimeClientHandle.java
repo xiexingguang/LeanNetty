82db7bdb6c2cec77264db5c3963d73c4ab45ed58
xiexg <568335129@qq.com>
2015-09-17 15:48:11 +0800

    对类TimeClientHandle进行补充

--- src/main/java/com/ec/xxg/server/TimeClientHandle.java
+++ src/main/java/com/ec/xxg/server/TimeClientHandle.java
@@ -1,7 +1,13 @@
 package com.ec.xxg.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by ecuser on 2015/9/16.
@@ -28,8 +34,79 @@ public class TimeClientHandle implements  Runnable{
 
     @Override
     public void run() {
+        try {
+            doConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> selectionKeyIterator = selectionKeys.iterator();
+                SelectionKey selectionKey = null;
+                while (selectionKeyIterator.hasNext()) {
+                    selectionKey = selectionKeyIterator.next();
+                    selectionKeyIterator.remove();
+
+                }
+            } catch (Exception e) {
+
+            }
+        }
     }
 
+    private void doConnect() throws IOException {
+        if (socketChannel.connect(new InetSocketAddress(host, port))) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        } else {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+    }
 
+    // 写字节
+    private  void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+        writeBuffer.put(req);
+        writeBuffer.flip();
+        socketChannel.write(writeBuffer);
+        if (!writeBuffer.hasRemaining()) {
+            System.out.println("send order 2 server success.....");
+        }
+    }
+
+     //deal with write..
+    private void handleInput(SelectionKey key) throws  IOException{
+        if (key.isValid()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                if (socketChannel.finishConnect()) {
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    doWrite(socketChannel);
+                } else {
+                    System.exit(1);
+                }
+            }
+            if (key.isReadable()) {
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "utf-8");
+                    System.out.println("NOW is =====>" + body);
+                    this.stop = true;
+                }else if (readBytes < 0) {
+                    key.cancel();
+                    socketChannel.close();
+                }else
+                    ;          // read 0 byte ,we ignore
+            }
+        }
+    }
 }
