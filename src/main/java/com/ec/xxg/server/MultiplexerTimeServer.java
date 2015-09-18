feebef59406fef30275ee84676c97ad9c83310e3
xiexg <568335129@qq.com>
2015-09-18 08:39:25 +0800

    启动客户端和服务端的启动类

--- src/main/java/com/ec/xxg/server/MultiplexerTimeServer.java
+++ src/main/java/com/ec/xxg/server/MultiplexerTimeServer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -21,17 +22,17 @@ public class MultiplexerTimeServer implements  Runnable{
              selector = Selector.open();
              serverSocketChannel = ServerSocketChannel.open();
              serverSocketChannel.configureBlocking(false);
-             serverSocketChannel.socket().bind(new InetSocketAddress(7070), 1024);
+             serverSocketChannel.socket().bind(new InetSocketAddress(7000), 1024);
              serverSocketChannel.register(selector,SelectionKey.OP_ACCEPT); //注册是否有客户端连接事件
-             System.out.print("The time server is  start in port : 7070");
+             System.out.print("The time server is  start in port : 7000");
          }catch (Exception e){
-             e.fillInStackTrace();
+             System.out.println(e);
              System.exit(8);
          }
     }
 
 
-    public void stop() {
+    public  void  stop() {
         this.stop = true;
     }
 
@@ -40,38 +41,63 @@ public class MultiplexerTimeServer implements  Runnable{
         while (!stop) {
             try {
                 selector.select(1000);
+
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (Throwable t) {
+                 t.printStackTrace();
+            }
+        }
+        if (selector != null) {
+            try {
+                selector.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            Iterator<SelectionKey> it = selectionKeys.iterator();
-            SelectionKey key = null;
-            while (it.hasNext()) {
-                key = it.next();
-                 it.remove();
 
-            }
         }
     }
 
     private void handleInput(SelectionKey key) throws IOException {
         if (key.isValid()) {
-              if(key.isAcceptable()){
+               if(key.isAcceptable()){
                   ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
                   SocketChannel socketChannel = serverSocketChannel.accept();
                   socketChannel.configureBlocking(false);
-                  socketChannel.register(selector,SelectionKey.OP_READ);
+                  socketChannel.register(selector,SelectionKey.OP_READ);   //listen the read event of the client
              }
             if (key.isReadable()) {
                 SocketChannel sc = (SocketChannel)key.channel();
                 ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                int readBytes = sc.read(byteBuffer);
+                int readBytes = 0;
+                if (sc != null) {
+                    readBytes  = sc.read(byteBuffer);
+                }
+
                 if (readBytes > 0) {
                     byteBuffer.flip();
                     byte[] bytes = new byte[byteBuffer.remaining()]; //Returns the number of elements between the current position and the limit
-                    byteBuffer.get(bytes);
+                    byteBuffer.get(bytes);                                      //将byteBuffer数据读取到bytes直接数组中去
                     String body = new String(bytes,"UTF-8");
-
+                    System.out.println("the server has been recieve  oreder:" + body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD ORDER";
+                    doWrite(sc, currentTime);
                 }else if (readBytes < 0) {
                      key.cancel();
                      sc.close();
@@ -83,12 +109,16 @@ public class MultiplexerTimeServer implements  Runnable{
     }
 
     private  void doWrite(SocketChannel socketChannel,String response) throws IOException {
-        if (response == null && response.trim().length() > 0) {
+        if (response != null && response.trim().length() > 0) {
             byte[] bytes = response.getBytes();
             ByteBuffer writeByteBuffer = ByteBuffer.allocate(bytes.length);
             writeByteBuffer.put(bytes);
             writeByteBuffer.flip();
             socketChannel.write(writeByteBuffer);
         }
+    }
+
+    public void testGi() {
+
     }
 }
